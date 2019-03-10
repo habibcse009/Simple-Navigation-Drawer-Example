3279b141a17b5571a2f54c56d29e4c4a7402c973
habib <habibcse009@gmail.com>
2019-03-10 15:38:49 +0600

    Simple navigation drawer and fragment and details activity with back button

---
+++ app/src/main/java/com/habibcse009/navigationdrawerandfragment/DetailsActivity.java
@@ -0,0 +1,30 @@
+package com.habibcse009.navigationdrawerandfragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        getSupportActionBar().setHomeButtonEnabled(true); //for back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//for back button
+        getSupportActionBar().setTitle("Fregment's Details");
+    }
+
+    //for back button
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
